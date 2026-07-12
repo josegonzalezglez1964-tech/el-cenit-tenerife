66de4907a6209eb835cb8c1822b6b29748edc9c8
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-07-12 22:36:30 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TestamentoWizard from "./pages/TestamentoWizard";
+import Boveda from "./pages/Boveda";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/testamento" element={<TestamentoWizard />} />
+      <Route path="/boveda" element={<Boveda />} />
     </Routes>
   );
 }
